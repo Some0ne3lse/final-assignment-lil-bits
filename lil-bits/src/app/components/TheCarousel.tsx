7325347dfbe8867eafe0ec377f3dd98286eab8dc
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-05-29 15:10:03 +0000

    Changed image carousel

--- lil-bits/src/app/components/TheCarousel.tsx
+++ lil-bits/src/app/components/TheCarousel.tsx
@@ -1,14 +1,14 @@
 "use client";
-import Image from "next/image";
-import React, { useEffect, useState } from "react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Image, { StaticImageData } from "next/image";
+import React, { useCallback, useEffect, useState } from "react";
+import UseEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+import image1 from "../../../public/carousel/first-carousel-image.webp";
+import image2 from "../../../public/carousel/second-carousel-image.webp";
+import image3 from "../../../public/carousel/third-carousel-image.webp";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 
-const carouselImages = [
-  "../../../public/carousel/first-carousel-image.webp",
-  "../../../public/carousel/second-carousel-image.webp",
-  "../../../public/carousel/third-carousel-image.webp",
-];
+const carouselImages = [image1, image2, image3];
 
 // const images = [
 //   "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/the-day-after-dish-katsudon-219e470.jpg?quality=90&webp=true&resize=440,400",
@@ -18,30 +18,60 @@ const carouselImages = [
 
 // TODO Change type
 
-const shuffle = (array: string[]) => [...array].sort(() => Math.random() - 0.5);
+const shuffle = (array: StaticImageData[]) =>
+  [...array].sort(() => Math.random() - 0.5);
 
 export default function TheCarousel() {
-  const [list, setList] = useState(carouselImages);
+  const [emblaRef, emblaApi] = UseEmblaCarousel({ loop: true }, [Autoplay()]);
+
+  const [imageList, setImageList] = useState(carouselImages);
   useEffect(() => {
     const mountArray = shuffle(carouselImages);
-    setList(mountArray);
+    setImageList(mountArray);
   }, []);
 
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
   return (
-    <div className="carouselBox">
-      <Carousel
-        useKeyboardArrows={true}
-        autoPlay
-        infiniteLoop
-        showThumbs={false}
-        showStatus={false}
+    <div className="embla">
+      <button
+        aria-label="go to previous slide"
+        onClick={scrollPrev}
+        className="carousel-button-left"
       >
-        {list.map((data: string, index: number) => (
-          <div key={index}>
-            <img alt="Image of one of our dishes" src={data} />
-          </div>
-        ))}
-      </Carousel>
+        <ChevronLeftIcon className="chevron-buttons" />
+      </button>
+      <button
+        aria-label="go to previous slide"
+        onClick={scrollNext}
+        className="carousel-button-right"
+      >
+        <ChevronRightIcon className="chevron-buttons" />
+      </button>
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {imageList.map((data, index) => (
+            <div className="embla__slide" key={index}>
+              <Image
+                className="carousel-images"
+                src={data}
+                width={0}
+                height={0}
+                sizes="100vw"
+                style={{ width: "100%", height: "auto", borderRadius: 8 }}
+                alt="Image of one of our dishes"
+                priority
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
